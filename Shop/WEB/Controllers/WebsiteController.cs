bef7507660d6a77f05864b31864674b1e0624f20
tinhpro248 <tinh.duongvdl.h@hcmut.edu.vn>
2021-05-29 17:01:49 +0700

    Update demo

---
+++ Shop/WEB/Controllers/WebsiteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        // GET: Website
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Website/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Website/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Website/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Website/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Website/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Website/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Website/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
